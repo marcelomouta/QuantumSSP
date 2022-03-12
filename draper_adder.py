9738226339cb5e7473ac584ba394a43af429637f
Marcelo Mouta <fc51110@alunos.fc.ul.pt>
2022-03-12 22:50:04 +0000

    implement draper adder

--- draper_adder.py
+++ draper_adder.py
@@ -1,6 +1,25 @@
 import pennylane as qml
+import math
 
 
-def adder():
-    # TODO
-    return
+def adder(wires_a, wires_b):
+    """
+    Drapper adder that transforms the states |a>|b> into |a>|a+b>
+
+    wires_a: indices of the wires containing value a
+    wires_a: indices of the wires containing value b
+
+    For further details see:
+      Ruiz-Perez et al., Quantum arithmetic with the Quantum Fourier Transform, 2017. https://arxiv.org/pdf/1411.5949.pdf
+    """
+
+    qml.QFT(wires=wires_b)
+
+    n = len(wires_b)
+
+    for j in range(n):
+        for k in range(n - j):
+            phi = math.pi / (2 ** k)
+            qml.ControlledPhaseShift(phi, wires=[wires_a[j + k], wires_b[-j - 1]])
+
+    qml.QFT(wires=wires_b).inv()
